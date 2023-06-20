7a04c9e042981e33278f315c7edf24b2debb370a
Nori Lacsina <toperrrrrrrr@gmail.com>
2023-06-20 19:04:42 +0800

    removed the bootstrap and made it regular CSS

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Form, Input, Alert, Card, Container, Stack } from 'react-bootstrap';
 import './mycss.css'
+import '../img/Logo.svg'
 
 const Login = () => {
   const [isButtonDisabled, setButtonDisabled] = useState('primary');
@@ -19,20 +20,20 @@ const Login = () => {
 
   return (
     <>
-    <Container className='loginbox'>
+    <div className='login-bg'>
       
-      <Card className='screen'>
-        
-        <Card.Body className='screen_content'>
+      <div className='login-box'>
+        <svg src='../img/Logo.svg'></svg>
+        <div className='screen_content'>
           <Form className='login '>  
             <input type='text' className='username'></input>
             <input type='password'></input> 
             <Button type='button' className='box btn' onClick={handleClick}><div className='loginbutton'>LOGIN</div></Button>
             <Alert variant='success' hidden={isAlertHidden}> Login Success </Alert>
           </Form>
-        </Card.Body>
-      </Card>
-    </Container>
+        </div>
+      </div>
+    </div>
     </>
   );
 };
