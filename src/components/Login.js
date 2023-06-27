ce1fe7812ae9b51205e888a8403ceea795061a70
Nori Lacsina <toperrrrrrrr@gmail.com>
2023-06-27 19:23:55 +0800

    made the login conditions work

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./mycss.css";
@@ -9,28 +10,60 @@ import bg from "../img/bg.svg";
 const Login = () => {
   const [isAlertHidden, setAlertHidden] = useState(true);
   const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isUsername, setUsername] = useState("");
+  const [isPassword, setPassword] = useState("");
+
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [isLogout, setLogout] = useState(false);
+
+  const exit = () => {
+    setLogout(true);
+    navigate("/");
+    if (isLogout) {
+      return <redirect to="/" />;
+    }
+  };
+
+  const toggleConfirmation = () => {
+    setShowConfirmation(!showConfirmation);
+  };
+
   const navigate = useNavigate();
+  let username = "admin"
+  let password = "password"
+  let sstatus = ""
 
   const handleLogin = () => {
+
+   if (username === isUsername && password === isPassword){
     setLoggedIn(true);
     navigate("/dashboard"); // Redirect to the dashboard page
     if (isLoggedIn) {
-      // Redirect to another page if logged in
       return <redirect to="/Dashboard" />;
     }
+  }
+  else{
+
+   toggleConfirmation()
+  }
+
   };
 
   return (
     <>
+      <Helmet>
+              <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        </Helmet>
       <div className="login-bg">
         <img src={bg} className="logo"></img>
         <div className="login-box">
           <div className="screen_content">
             <Form className="login ">
+              <h4> {sstatus}</h4>
               <h4> Username</h4>
-              <input type="text" className="username"></input>
+              <input type="text" className="username"  onChange={(e) => setUsername(e.target.value)}></input>
               <h4> Password</h4>
-              <input type="password"></input>
+              <input type="password"  onChange={(e) => setPassword(e.target.value)} ></input>
               <div className="confirmation-buttons"> 
               <Button type="button" className="box btn" onClick={handleLogin}>
                 <div className="loginbutton">LOGIN</div>
@@ -41,6 +74,20 @@ const Login = () => {
           </div>
         </div>
       </div>
+
+      <div>
+          {showConfirmation && (
+            <div className="confirmation-overlay" onClick={toggleConfirmation}>
+              <div className="confirmation-box">
+                <h2>incorrect Credentials</h2>  
+               
+                <div className="confirmation-buttons">
+                  <button onClick={toggleConfirmation} className="box btn">Exit</button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
     </>
   );
 };
