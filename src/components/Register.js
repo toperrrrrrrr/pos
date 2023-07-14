10d2bc30a760a07dcb00fd836af271b64bbb748c
Nori Lacsina <toperrrrrrrr@gmail.com>
2023-07-14 15:51:09 +0800

    insert into mysql success

--- src/components/Register.js
+++ src/components/Register.js
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./mycss.css";
 import bg from "../img/bg.svg";
+import Axios from 'axios'
+
 
 const Register = () => {
 
@@ -13,14 +15,17 @@ const Register = () => {
 
 
 
-  const handleRegister = () => {
-   
+
+  const handleSubmit = () => {
+   Axios.post('http://localhost:3001/api/insert', {userName: isUsername, password: isPassword} ).then(()=>{
+
+   alert("Successfull insert")
+   })
   };
 
   const handleNavigateLogin = () => {
   navigate("/");
   return <redirect to="/Login" />;
-
   };
   
   return (
@@ -30,13 +35,13 @@ const Register = () => {
         <div className="login-box">
           <div className="screen_content">
             <Form className="login ">
-              <h4> Username</h4>
-              <input type="text" className="username" value={isUsername}  onChange={(e) => setUsername(e.target.value)}></input>
-              <h4> Password</h4>
+              <h4> Username {isUsername} </h4>
+              <input type="text" className="username"  onChange={(e) => setUsername(e.target.value)}></input>
+              <h4> Password {isPassword} </h4>
               <input type="password" onChange={(e) => setPassword(e.target.value)} ></input>
               <div className="confirmation-buttons"> 
-              <Button type="button" className="box btn" onClick={handleRegister}>
-                <div className="loginbutton">Register</div>
+              <Button type="button" className="box btn" onClick={handleSubmit}>
+                <div className="loginbutton">Submit</div>
               </Button>
               <Button type="button" className="box btn" onClick={handleNavigateLogin}>
                 <div className="loginbutton">Login</div>
